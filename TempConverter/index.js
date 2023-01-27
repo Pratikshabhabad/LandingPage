79dfb0732a602a54a67f7a43150306e117d3b80a
Pratiksha Bhabad <92775102+Pratikshabhabad@users.noreply.github.com>
2023-01-27 19:45:17 +0530

    Add files via upload

---
+++ TempConverter/index.js
@@ -0,0 +1,32 @@
+const celciusInput=document.getElementById("celcius");
+const fahrenhitInput=document.getElementById("fahrenhit");
+const kelvinInput=document.getElementById("kelvin");
+
+const inputs=document.getElementsByClassName("inputs");
+for(let i=0;i<inputs.length;i++)
+{
+    let input =inputs[i];
+
+    input.addEventListener("input",function(e){
+
+        let value=parseFloat(e.target.value);
+        
+
+        switch(e.target.name)
+        {
+            case "celcius":
+                fahrenhitInput.value=(value*1.8)+32;
+                kelvinInput.value=value+273.15
+            break;
+
+            case "fahrenhit":
+                celciusInput.value=(value-32)/1.8;
+                kelvinInput.value=((value-32)/1.8)+273.15;
+            break;
+            case "kelvin":
+            celciusInput.value=value-273.15;
+            fahrenhitInput.value=((value-273.15)*1.8)+32;
+            break;
+        }
+    })
+}
